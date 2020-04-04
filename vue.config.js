3bb6c1248e03fbc17d1a967319028a3615e03428
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 15:05:00 +0800

    fixed sw.js

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,6 @@ module.exports = {
   // lintOnSave: false,
   pwa: {
     name: '北科監控雲',
-    // manifestOptions:{
-    //   name: pwa.name,
-    //   short_name: pwa.name,
-    //   start_url: '.',
-    //   display: 'standalone',
-    //   theme_color: pwa.themeColor
-    // },
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
@@ -19,7 +12,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'src/sw.js',
+      swSrc: 'src/ServiceWorker.js',
     }
   }
 }
