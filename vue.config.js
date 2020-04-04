5972db86781503a916e9e0358a18333649da0917
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 14:52:47 +0800

    push to githubPage

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,12 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
+    start_url: 'http://localhost:8080/#/',
     // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: '/src/sw.js',
-      // ...other Workbox options...
+      swSrc: 'src/sw.js',
     }
   }
 }
