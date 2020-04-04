a7e2810ceed82297478163697ff6facbf4a46b87
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 15:07:05 +0800

    service-worker.js

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'src/ServiceWorker.js',
+      swSrc: 'src/service-worker.js',
     }
   }
 }
