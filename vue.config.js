d55f1cf4fd56b89789692c63f010964aee95df9c
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 14:23:23 +0800

    fixed some problem

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  lintOnSave: false
-}
-module.exports = {
+  // lintOnSave: false,
   pwa: {
     name: '北科監控雲',
-    manifestOptions:{
-      name: pwa.name,
-      short_name: pwa.name,
-      start_url: '.',
-      display: 'standalone',
-      theme_color: pwa.themeColor
-    },
+    // manifestOptions:{
+    //   name: pwa.name,
+    //   short_name: pwa.name,
+    //   start_url: '.',
+    //   display: 'standalone',
+    //   theme_color: pwa.themeColor
+    // },
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
@@ -20,7 +18,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: '/sw.js',
+      swSrc: '/src/sw.js',
       // ...other Workbox options...
     }
   }
