80422e427b54ac21e60d5b1562455053dcfc427b
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 17:15:52 +0800

    solved some problem for load js file

---
+++ dist/precache-manifest.a603620bebc224208ee69459750f0ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281ee6b7f65a865908d8",
+    "url": "css/app.d0656c5e.css"
+  },
+  {
+    "revision": "8d86250348561781dd7f91e7d8d5c4ea",
+    "url": "index.html"
+  },
+  {
+    "revision": "281ee6b7f65a865908d8",
+    "url": "js/app.8b6b9325.js"
+  },
+  {
+    "revision": "88bcf0754b9b4bec5cb5",
+    "url": "js/chunk-vendors.f58cfb9f.js"
+  },
+  {
+    "revision": "e1376d606c7053b9ba42b2334bbbb723",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
