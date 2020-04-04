b4b1c65ff553c8fd30ebb352e02ffed8f3e4bbc5
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 14:02:08 +0800

    fixed pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   lintOnSave: false
 }
+module.exports = {
+  pwa: {
+    name: '北科監控雲',
+    manifestOptions:{
+      name: pwa.name,
+      short_name: pwa.name,
+      start_url: '.',
+      display: 'standalone',
+      theme_color: pwa.themeColor
+    },
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: '/sw.js',
+      // ...other Workbox options...
+    }
+  }
+}
