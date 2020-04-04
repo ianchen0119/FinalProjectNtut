b10556eefc4b15a6b26171d8a041d6884f8dd3ed
Ian Chen <iancodinghtml@gmail.com>
2020-04-04 15:01:20 +0800

    fixed issue for path

---
+++ dist/precache-manifest.18ac1f62445088ab981a0d89c4508f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5f7e0fa490dc87eece",
+    "url": "css/app.d0656c5e.css"
+  },
+  {
+    "revision": "9c59643fdab527dcb474a6b691b3c2d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "2e5f7e0fa490dc87eece",
+    "url": "js/app.fddd4298.js"
+  },
+  {
+    "revision": "88bcf0754b9b4bec5cb5",
+    "url": "js/chunk-vendors.f58cfb9f.js"
+  },
+  {
+    "revision": "a710f9b4a2a9956b5d1329b15e901832",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
