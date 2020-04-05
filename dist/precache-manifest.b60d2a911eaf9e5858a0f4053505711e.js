9169fcac24418b031fbf4460d3e965946e0e84b6
Ian Chen <iancodinghtml@gmail.com>
2020-04-05 20:10:12 +0800

    fixed some bug

---
+++ dist/precache-manifest.b60d2a911eaf9e5858a0f4053505711e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c8f5b6028a513929bd",
+    "url": "css/app.ada48fa0.css"
+  },
+  {
+    "revision": "7943c0bf93bb0a9764ac73f42ce56930",
+    "url": "index.html"
+  },
+  {
+    "revision": "92c8f5b6028a513929bd",
+    "url": "js/app.6384de04.js"
+  },
+  {
+    "revision": "88bcf0754b9b4bec5cb5",
+    "url": "js/chunk-vendors.f58cfb9f.js"
+  },
+  {
+    "revision": "e1376d606c7053b9ba42b2334bbbb723",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
